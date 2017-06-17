b72be1d345fda6a1a1a1434960b478f61c6fe0aa
Thomas Cheng <thomascheng81@gmail.com>
2017-06-16 23:21:46 -0400

    Add correct tile rendering

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,17 +1,34 @@
 class Rectangle {
-  constructor ({ width, height, x, y, fill }) {
+  constructor({ width, height, x, y, fill, radius }) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.radius = radius || 0;
     this.fill = fill;
   }
 
   render = context => {
     context.beginPath();
-    context.rect(this.x, this.y, this.width, this.height);
     context.fillStyle = this.fill;
-    context.fill();
+    context.strokeStyle = this.fill;
+    context.lineJoin = "round";
+    context.lineWidth = this.radius;
+
+    context.strokeRect(
+      this.x + this.radius / 2,
+      this.y + this.radius / 2,
+      this.width - this.radius,
+      this.height - this.radius
+    );
+    context.fillRect(
+      this.x + this.radius / 2,
+      this.y + this.radius / 2,
+      this.width - this.radius,
+      this.height - this.radius
+    );
+    // context.rect(this.x, this.y, this.width, this.height);
+    // context.fill();
   };
 }
 
--- src/tile.js
+++ src/tile.js
@@ -1,12 +1,15 @@
 import Rectangle from "./rectangle";
 
-const PADDING = 10;
+const PADDING = 16;
+const RADIUS = 10;
 const DECAY = 0.3;
 
 class Tile {
-  constructor({ i, j, width, height, color }) {
+  constructor({ i, j, width, height, color, iFinal, jFinal }) {
     this.i = i;
     this.j = j;
+    this.iFinal = iFinal;
+    this.jFinal = jFinal;
     this.width = width;
     this.height = height;
     this.position = null;
@@ -16,6 +19,7 @@ class Tile {
       y: j * height + 0.5 * PADDING,
       width: width - PADDING,
       height: height - PADDING,
+      radius: RADIUS,
       fill: color
     });
   }
@@ -40,25 +44,37 @@ class Tile {
 
   swap = otherTile => {
     const { i: iOther, j: jOther } = otherTile;
-    otherTile.i = this.i;
-    otherTile.j = this.j;
-    this.i = iOther;
-    this.j = jOther;
+
+    if (!otherTile.isCorrect()) {
+      otherTile.i = this.i;
+      otherTile.j = this.j;
+      this.i = iOther;
+      this.j = jOther;
+    }
   };
 
+  isCorrect = () => this.i === this.iFinal && this.j === this.jFinal;
+
   update = () => {
+    const isCorrect = this.isCorrect();
+    const padding = isCorrect ? 0 : PADDING;
+    const radius = isCorrect ? 0 : RADIUS;
+
     if (this.position) {
       this.rectangle.x = this.position.x;
       this.rectangle.y = this.position.y;
     } else {
-      this.rectangle.x = this.rectangle.x + DECAY * (this.i * this.width + 0.5 * PADDING - this.rectangle.x);
-      this.rectangle.y = this.rectangle.y + DECAY * (this.j * this.height + 0.5 * PADDING - this.rectangle.y);
+      this.rectangle.x = this.rectangle.x + DECAY * (this.i * this.width + 0.5 * padding - this.rectangle.x);
+      this.rectangle.y = this.rectangle.y + DECAY * (this.j * this.height + 0.5 * padding - this.rectangle.y);
       this.rectangle.width =
         this.rectangle.width +
-        DECAY * (this.width - PADDING - this.rectangle.width);
+        DECAY * (this.width - padding - this.rectangle.width);
       this.rectangle.height =
         this.rectangle.height +
-        DECAY * (this.height - PADDING - this.rectangle.height);
+        DECAY * (this.height - padding - this.rectangle.height);
+      this.rectangle.radius =
+        this.rectangle.radius +
+        DECAY * (radius - this.rectangle.radius);
     }
   };
 
