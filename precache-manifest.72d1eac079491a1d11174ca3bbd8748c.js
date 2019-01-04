01329eed69f72ff5a8e6f7030dad9d4ea1f1b960
Thomas Cheng <thomascheng81@gmail.com>
2019-01-04 08:51:44 -0500

    Updates

---
+++ precache-manifest.72d1eac079491a1d11174ca3bbd8748c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "41bd467a90d36e6aec99",
+    "url": "/static/js/main.41bd467a.chunk.js"
+  },
+  {
+    "revision": "edea5ff6025306845548",
+    "url": "/static/js/1.edea5ff6.chunk.js"
+  },
+  {
+    "revision": "41bd467a90d36e6aec99",
+    "url": "/static/css/main.ca4426ce.chunk.css"
+  },
+  {
+    "revision": "0a9bfbda0c4b9574eb2c45325d24379e",
+    "url": "/index.html"
+  }
+];
